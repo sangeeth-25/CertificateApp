7451e01c5dcc8575e0d9837e3205fd43961a5e5e
Your Name <you@example.com>
2024-08-08 12:48:31 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,23 @@
 import express, { json } from 'express';
+import cors from 'cors';
 const listen_port=8000;
 import userRouter from './user-routes.js';
 const app = express()
 app.use(express.json());
+//app.use(cors({ origin: "*" }));
 
+app.use(cors({origin:"*"}))
 app.use(json());
+app.use(express.static('public'));
 
-
-app.use("/", userRouter);
+ app.use("/", userRouter);
  
 
 app.listen((listen_port),()=>{
     console.log(`App listening to port ${listen_port}`);
 })
 
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
