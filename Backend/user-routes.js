53b34479acec4c1d22c2a4328aca5da55e233619
Your Name <you@example.com>
2024-08-08 12:24:16 +0530

    Updated user-routes

--- Backend/user-routes.js
+++ Backend/user-routes.js
@@ -11,16 +11,7 @@ router.post('/issue', async (req, res) => {
     try {
 
 
-        const { certificateID, courseName, candidateName, grade, date } = req.body;
-        console.log(certificateID);
-        if (certDetails.has(certificateID)) {
-            res.status(201).json({ message: `${certificateID} already exist` })
-        }
-        else {
-            certDetails.set(certificateID, { courseName, candidateName, grade, date });
-            console.log(certDetails.get(certificateID));
-            res.status(201).json({ message: 'Certificate details saved' })
-        }
+        
     } catch (error) {
         console.log(error);
         res.status(500).json();
@@ -28,19 +19,7 @@ router.post('/issue', async (req, res) => {
 })
 
 router.get('/view/:id', async (req, res) => {
-    const id1 = req.params.id;
-    console.log(id1);
-    const details = certDetails.get(id1);
-    console.log(details);
-
-    if (details) {
-
-        res.json(details);
-    }
-    else {
-
-        res.status(404).json({ message: 'details not found' });
-    }
+    
 
 })
 
