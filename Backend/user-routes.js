322a7a7bc873a404f1f3513f9abf50095b63ead5
sangeeth-25 <sansunny.s96@gmail.com>
2025-01-12 12:59:35 +0530

    frontend

--- Backend/user-routes.js
+++ Backend/user-routes.js
@@ -3,11 +3,22 @@ var router = Router();
 const certDetails = new Map();
 
 router.get('/', (req, res) => {
+    console.log("hi");
     res.send("Hello World");
 })
 
 router.post('/issue', async (req, res) => {
     try {
+        const { certificateID, courseName, candidateName, grade, date } = req.body;
+        console.log(certificateID);
+        if (certDetails.has(certificateID)) {
+            res.status(200).json({ message: `${certificateID} already exist` })
+        }
+        else {
+            certDetails.set(certificateID, { courseName, candidateName, grade, date });
+            console.log(certDetails.get(certificateID));
+            res.status(201).json({ message: 'Certificate details saved' })
+        }
 
 
         
@@ -18,6 +29,16 @@ router.post('/issue', async (req, res) => {
 })
 
 router.get('/view/:id', async (req, res) => {
+    const id1 = req.params.id;
+    console.log(id1);
+    const details = certDetails.get(id1);
+    console.log(details);
+    if (details) {
+        res.json(details);
+    }
+    else {
+        res.status(404).json({ message: 'details not found' });
+    }
     
 
 })
